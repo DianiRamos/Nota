43c9e4d73771d2e90a1521fae825cb5056291896
Diana Ramos <agui_diana@hotmail.com>
2017-11-07 11:50:42 -0600

    Initial commit

---
+++ app/src/main/java/com/ambrosio/ramos/nota/MainActivity.java
@@ -0,0 +1,22 @@
+package com.ambrosio.ramos.nota;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btnEntrar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,Principal.class);
+                startActivity(i);
+            }
+        });
+    }
+}
